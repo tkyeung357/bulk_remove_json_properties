815eb914813bd36c4ef511ac8f10a245060695d4
Tim Yeung <timyeung357@me.com>
2019-03-19 13:00:50 +0800

    support 1 dimension remove

--- bulk_remove_json_props.php
+++ bulk_remove_json_props.php
@@ -1,18 +1,23 @@
 <?php 
-//read json from file
-$myJsonStr = file_get_contents('myJson.json');
+$scope = $argv[1];
+$property = $argv[2];
+$input = $argv[3];
+$output = $argv[4];
 
-//convert to array
+$myJsonStr = file_get_contents("{$input}.json");
+
 $myJson = json_decode($myJsonStr, true);
 
-$myJson = array_map(function($d){
-    //remove preperty
-    unset($d[0]['Timestamp']);
-    return $d;
-}, $myJson);
+//support 1 dimension for now
+if (array_key_exists($scope, $myJson)) {
+    //scope found
+    $myJson[$scope] = array_map(function($d){
+        //remove property
+        unset($d['Timestamp']);
+        return $d;
+    }, $myJson[$scope]);
 
-print_r(json_encode($myJson));
+    print_r(json_encode($myJson));
 
-//save result to yourJson.json
-file_put_contents('yourJson.json', json_encode($myJson));
-
+    file_put_contents("{$input}.json", json_encode($myJson));
+}
