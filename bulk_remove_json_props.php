62670e3cc219796767d4ddb04792f5b5b86a9fd8
Tim Yeung <timyeung357@me.com>
2019-03-19 12:35:08 +0800

    add save result to file

--- bulk_remove_json_props.php
+++ bulk_remove_json_props.php
@@ -1,12 +1,18 @@
 <?php 
-
+//read json from file
 $myJsonStr = file_get_contents('myJson.json');
 
+//convert to array
 $myJson = json_decode($myJsonStr, true);
+
 $myJson = array_map(function($d){
+    //remove preperty
     unset($d[0]['Timestamp']);
     return $d;
 }, $myJson);
 
 print_r(json_encode($myJson));
+
+//save result to yourJson.json
+file_put_contents('yourJson.json', json_encode($myJson));
 
